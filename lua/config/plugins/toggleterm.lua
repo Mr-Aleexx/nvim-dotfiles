2fe42e608fb0c06748fda62ba46b5a29fbaecc96
Hazet Alex <alex.hazet@gmail.com>
2026-03-31 16:37:26 +0200

    toggleterm keybinds

--- lua/config/plugins/toggleterm.lua
+++ lua/config/plugins/toggleterm.lua
@@ -4,4 +4,5 @@ return {
   config = true,
 
   vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm direction=vertical size=50<CR>', { desc = 'Launch floating terminal' }),
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>h]], { noremap = true }),
 }
