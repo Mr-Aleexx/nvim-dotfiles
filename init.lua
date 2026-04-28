d9e38435af886f075a865fe946d168e0237513be
Alex HAZET <alex.hazet@gmail.com>
2026-04-28 14:38:08 +0200

    Telescope + tree qol changes + keybinds opts

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@ require("config.lazy")
 require("config.keymaps")
 vim.opt.shiftwidth = 4
 vim.opt.number = true
+
--- lua/config/plugins/tree.lua
+++ lua/config/plugins/tree.lua
@@ -61,5 +61,16 @@ return {
     keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Toggle file explorer on current file' }) -- toggle file explorer on current file
     keymap.set('n', '<leader>ec', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse file explorer' }) -- collapse file explorer
     keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' }) -- refresh file explorer
+
+
+    -- quit on unfocused
+    vim.api.nvim_create_autocmd("WinLeave", {
+	callback = function()
+	    if vim.bo.filetype == "NvimTree" then
+		require("nvim-tree.api").tree.close()
+	    end
+	end,
+    })
   end,
+
 }
