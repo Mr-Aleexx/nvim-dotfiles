1caa6dbc464d083161c85a17104ebbd0d0544c2a
HAZET Alex <alex.hazet@gmail.com>
2026-04-29 08:14:01 +0200

    Keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
+local builtin = require("telescope.builtin")
+
 -- Overrides the nvim base with nvim-tree
 vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
@@ -15,3 +17,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set("n", "<leader>h", "5<C-w><", { desc = "Decrease window width" })
 vim.keymap.set("n", "<leader>l", "5<C-w>>", { desc = "Increase window width" })
+
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep,  { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers,    { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags,  { desc = 'Telescope help tags' })
